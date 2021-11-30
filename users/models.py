1ddb9214fb989043d1850f26813c7de900b27076
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2021-11-30 18:24:23 +0900

    2nd revision_models.py

--- users/models.py
+++ users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-    name        = models.CharField(max_length= 100)
-    email       = models.CharField(max_length=250 , unique= True)
-    password    = models.CharField(max_length=300)
-    address     = models.CharField(max_length=100)
-    created_at  = models.DateTimeField(auto_now_add=True)
-    updated_at  = models.DateTimeField(auto_now=True)
+    name         = models.CharField(max_length=100)
+    email        = models.CharField(max_length=250, unique=True)
+    password     = models.CharField(max_length=300)
+    address      = models.CharField(max_length=100)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+    vegan_or_not = models.BooleanField(default=False)
     
     class Meta:
         db_table = 'users'
