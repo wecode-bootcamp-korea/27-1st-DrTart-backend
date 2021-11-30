d8d01bff50bc6871016b9d98b9f8fafcf57e8aba
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2021-11-30 19:30:15 +0900

    4th revision_models.py

--- orders/models.py
+++ orders/models.py
@@ -16,8 +16,8 @@ class OrderStatus(models.Model):
         
 class Order(models.Model):
     user         = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    created_at   = models.DateTimeField(auto_now_add=True)
-    updated_at   = models.DateTimeField(auto_now=True)
+    created_at   = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at   = models.DateTimeField(auto_now=True, null=True)
     address      = models.CharField(max_length=100)
     order_status = models.ForeignKey('OrderStatus', on_delete=models.CASCADE)
     
@@ -28,8 +28,8 @@ class OrderItem(models.Model):
     order        = models.ForeignKey('Order', on_delete=models.CASCADE)
     product      = models.ForeignKey('products.Product', on_delete=models.CASCADE)
     quantity     = models.IntegerField(default=1)
-    created_at   = models.DateTimeField(auto_now_add=True)
-    updated_at   = models.DateTimeField(auto_now=True)
+    created_at   = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at   = models.DateTimeField(auto_now=True, null=True)
     
     class Meta:
         db_table = 'order_items'
--- products/models.py
+++ products/models.py
@@ -34,6 +34,8 @@ class Review(models.Model):
     user        = models.ForeignKey(User, on_delete=models.CASCADE)
     product     = models.ForeignKey(Product, on_delete=models.CASCADE)
     content     = models.CharField(max_length=200, null=True)
+    created_at  = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at  = models.DateTimeField(auto_now=True, null=True)
     
     class Meta:
         db_table='reviews'
