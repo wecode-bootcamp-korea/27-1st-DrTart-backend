2fa66f99b747ba57d22c576a0f095142c03ea1ec
joshua <krjoshua21@gmail.com>
2021-12-06 18:44:47 +0900

    add signin

--- users/views.py
+++ users/views.py
@@ -36,4 +36,27 @@ class SignupView(View):
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
 
+class SigninView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user = User.objects.filter(email = data['email'])
 
+            if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                raise ValidationError('INVALID_USER')
+
+            token=jwt.encode({'user':'user.id'},SECRET_KEY,algorithm=ALGORITHM)
+
+            return JsonResponse({
+                'message':'SUCCESS',
+                'access_token':token
+            },status=200)
+            
+        except User.DoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status=401)
+        
+        except ValidationError as e:
+            return JsonResponse({'message' : e.message}, status = 401)
+        
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status = 400)
