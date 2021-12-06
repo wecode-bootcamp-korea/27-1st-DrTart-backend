ffdbeef4843545279e33bb7163331943aa24d3a5
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2021-12-06 19:15:34 +0900

    Model: menu meta table created : menu meta table created

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ from users.models  import User
 
 class Menu(models.Model):
     name = models.CharField(max_length=20)
+    
+    class Meta:
+        db_table='menu'
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
