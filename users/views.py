57c992d85bb22908be4ea7525966e2d608d522f1
joshua <krjoshua21@gmail.com>
2021-12-07 21:59:16 +0900

    /error clear

--- users/views.py
+++ users/views.py
@@ -41,12 +41,12 @@ class SigninView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            user = User.objects.filter(email = data['email'])
+            user = User.objects.get(email = data['email'])
 
             if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
                 return ValidationError('INVALID_USER')
 
-            token=jwt.encode({'user':'user.id'},SECRET_KEY,algorithm=ALGORITHM)
+            token=jwt.encode({'user':user.id},SECRET_KEY,algorithm=ALGORITHM)
 
             return JsonResponse({
                 'message':'SUCCESS',
