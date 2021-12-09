05fd3914c59c7f317b4e9fb20a87befb9ee1475a
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2021-12-09 14:36:55 +0900

    ADDED: Order Page added

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views      import CartView
+from .views      import CartView, OrderView
 
 urlpatterns = [
     path('/cart', CartView.as_view()),
+    path('/checkout', OrderView.as_view()),
 ]
--- orders/views.py
+++ orders/views.py
@@ -73,3 +73,77 @@ class CartView(View):
         cart.delete()
 
         return JsonResponse({"message" : "DELETE_SUCCESS"}, status=204)
+    
+class OrderView(View):
+    @authorization
+    def post(self, request):
+
+        data     = json.loads(request.body)
+        user     = request.user
+        items    = data
+
+        try: 
+            with transaction.atomic():
+                PENDING = OrderStatus.objects.get(id=2)
+                    
+                order = Order.objects.create(  
+                    address      = User.objects.get(id=user.id).address,
+                    user         = user,
+                    order_status = PENDING
+                )   
+                
+                order_items = [OrderItem(
+                    order      = order,
+                    product_id = item['product_id'],
+                    quantity   = item['quantity']
+                ) for item in items]
+                
+                OrderItem.objects.bulk_create(order_items)
+            
+                carts = Cart.objects.filter(user = user)                
+                carts.delete() 
+
+            return JsonResponse({'message':'SUCCESS'}, status=201)
+        
+        except IntegrityError:
+            return JsonResponse({'message':'INTEGRITY_ERROR'}, status=401)                                                                                                                      
+        
+    @authorization
+    def get(self, request):
+        user = request.user
+        order = Order.objects.filter(user=user).order_by('-created_at')[0]
+        order_items = order.orderitem_set.all()
+        
+        total_price_list = []
+        for order_item in order_items:
+            a = order_item.quantity * order_item.product.price
+            total_price_list.append(a)
+        
+
+        order_list = [{
+            'order_id'     : order.id,
+            'user'         : User.objects.get(id=user.id).name,
+            'address'      : order.address,
+            'order_items'  : [{
+                'product_id'    : order_item.product.id,
+                'quantity'      : order_item.quantity,
+                'product_name'  : order_item.product.korean_name,
+                'product_image' : order_item.product.thumbnail_image_url,
+                'price'         : int((order_item.quantity) * (order_item.product.price))
+            } for order_item in order_items],
+            'total_price'  : int(sum(total_price_list))
+        }]
+        
+        return JsonResponse({'order_list':order_list}, status=200)
+
+    @authorization
+    def patch(self, request):
+        data = json.loads(request.body)
+        order_id = data['order_id']  
+        
+        CANCELED = OrderStatus.objects.filter(id=6)
+        
+        if order_id:
+            Order.objects.filter(id=order_id).update(order_status=CANCELED)
+            
+        return JsonResponse({'message':'SUCCESS'}, status=200)
