a48d7196ada784084997f0c10c8ae8c835137863
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2021-12-10 18:09:16 +0900

    Modifided

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from products.views import ProductView, ProductDetailView
+from products.views import ProductView, ProductDetailView, LikeView
 
 
 urlpatterns = [
     path('', ProductView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
+    path('/<int:product_id>/like', LikeView.as_view()),
 ]
